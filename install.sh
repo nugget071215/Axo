2e160c87eb4cbc7ea5737d50c0386c66355bb2fe
nugget <averybura8@gmail.com>
2026-06-29 13:27:08 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.local/bin
+
+cd Axo
+
+mv axo ~
+
+sudo mv ~/Axo /etc/nixos
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' >>"$HOME/.bashrc"
+
+echo "Put ./axo/axo.nix in the imports section of your configuration.nix file and rebuild to finish installation."
